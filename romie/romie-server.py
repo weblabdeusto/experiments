f2316b0849e3b48995e7217e5830709e23fa8f8e
Razican <admin@razican.com>
2015-06-18 17:14:48 +0200

    Indentation changed to spaces

--- romie/romie-server.py
+++ romie/romie-server.py
@@ -17,88 +17,88 @@
 # TODO check "S"'s return
 
 try:
-	import bluetooth
+    import bluetooth
 
-	BT_address = '00:12:02:09:05:16'
-	BT_port = 1
+    BT_address = '00:12:02:09:05:16'
+    BT_port = 1
 
-	BT_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-	BT_socket.connect((BT_address, BT_port))
+    BT_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    BT_socket.connect((BT_address, BT_port))
 
-	BT_available = True
+    BT_available = True
 except:
- 	BT_available = False
- 	print 'No bluetooth device is available'
+    BT_available = False
+    print 'No bluetooth device is available'
 
 class RoMIE:
-	def _wait_ack(self):
-		if not BT_available: return
+    def _wait_ack(self):
+        if not BT_available: return
 
-		received = ''
-		while 'ACK' not in received and 'NAK' not in received:
-			received += BT_socket.recv(1024)
-		if 'NAK' in received:
-			pass
+        received = ''
+        while 'ACK' not in received and 'NAK' not in received:
+            received += BT_socket.recv(1024)
+        if 'NAK' in received:
+            pass
 
-		return received
+        return received
 
-	def forward(self):
-		if not BT_available: return 'Bluetooth error'
+    def forward(self):
+        if not BT_available: return 'Bluetooth error'
 
-		BT_socket.send('F')
-		response = self._wait_ack()
-		return response
+        BT_socket.send('F')
+        response = self._wait_ack()
+        return response
 
-	def turn_left(self):
-		if not BT_available: return 'Bluetooth error'
+    def turn_left(self):
+        if not BT_available: return 'Bluetooth error'
 
-		BT_socket.send('L')
-		response = self._wait_ack()
-		return response
+        BT_socket.send('L')
+        response = self._wait_ack()
+        return response
 
-	def turn_right(self):
-		if not BT_available: return 'Bluetooth error'
+    def turn_right(self):
+        if not BT_available: return 'Bluetooth error'
 
-		BT_socket.send('R')
-		response = self._wait_ack()
-		return response
+        BT_socket.send('R')
+        response = self._wait_ack()
+        return response
 
-	def check_wall(self):
-		if not BT_available: return 'Bluetooth error'
+    def check_wall(self):
+        if not BT_available: return 'Bluetooth error'
 
-		BT_socket.send('S')
-		response = self._wait_ack()
-		return response
+        BT_socket.send('S')
+        response = self._wait_ack()
+        return response
 
 import BaseHTTPServer
 import urlparse
 
 class RoMIEHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
-	def __init__(self,req,client_addr,server):
-		BaseHTTPServer.BaseHTTPRequestHandler.__init__(self,req,client_addr,server)
+    def __init__(self,req,client_addr,server):
+        BaseHTTPServer.BaseHTTPRequestHandler.__init__(self,req,client_addr,server)
 
-	def do_GET(self):
-		parsedParams = urlparse.urlparse(self.path)
-		queryParsed = urlparse.parse_qs(parsedParams.query)
+    def do_GET(self):
+        parsedParams = urlparse.urlparse(self.path)
+        queryParsed = urlparse.parse_qs(parsedParams.query)
 
-		global romie;
-		response = '';
+        global romie;
+        response = '';
 
-		if parsedParams.path == "/l":
-			response = romie.turn_left()
-		elif parsedParams.path == "/r":
-			response = romie.turn_right()
-		elif parsedParams.path == "/f":
-			response = romie.forward()
-		elif parsedParams.path == "/s":
-			response = romie.check_wall()
+        if parsedParams.path == "/l":
+            response = romie.turn_left()
+        elif parsedParams.path == "/r":
+            response = romie.turn_right()
+        elif parsedParams.path == "/f":
+            response = romie.forward()
+        elif parsedParams.path == "/s":
+            response = romie.check_wall()
 
-		if response is not '' or True:
-			self.send_response(200)
-			self.send_header('Content-Type', 'text/html')
-			self.end_headers()
-			self.wfile.write(response)
+        if response is not '' or True:
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+            self.wfile.write(response)
 
 romie = RoMIE()
 httpd = BaseHTTPServer.HTTPServer(('', 8000), RoMIEHandler)

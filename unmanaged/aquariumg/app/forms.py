f046ca94d7d14b3b893e338e7bad29b692eaa9ac
Gabi <gabimartinez@opendeusto.es>
2015-10-06 20:52:15 +0200

    files removed

--- unmanaged/aquariumg/app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-
-class LoginForm(Form):
-    name = StringField('name', validators=[DataRequired()])
-
-
--- unmanaged/microscope/runp.py
+++
@@ -1,4 +0,0 @@
-
-from app import app
-
-app.run('0.0.0.0', debug=False, port=8080)

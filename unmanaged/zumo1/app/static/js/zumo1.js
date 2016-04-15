4234a29df6169c3138921513dcb1afadbf0a1112
gabimart <gabimartinez@opendeusto.es>
2016-04-15 17:22:00 +0200

    .

--- unmanaged/zumo1/app/static/js/zumo1.js
+++ unmanaged/zumo1/app/static/js/zumo1.js
@@ -136,7 +136,47 @@ $(document).ready(function(){
     file_manager = new FileManager();
     file_manager.init();
 
+    //socketsss
+    namespace = '/zumo_backend'; // change to an empty string to use the global namespace
+
+    // the socket.io documentation recommends sending an explicit package upon connection
+    // this is specially important when using the global namespace
+    var socket = io.connect('http://' + document.domain + ':' + location.port + namespace);
+
+    // event handler for server sent data
+    // the data is displayed in the "Received" section of the page
+    socket.on('Serial data', function(msg) {
+        var serialDiv = $('#serial-monitor');
+        serialDiv.append('<p>'+msg.data+'</p>');
+        serialDiv.scrollTop(serialDiv.children().length*1000)
+    });
+
+    socket.on('General', function(msg) {
+        var serialDiv = $('#serial-monitor');
+        serialDiv.append('<p>General: ' + msg.data+'</p>');
+        serialDiv.scrollTop(serialDiv.children().length*1000)
+    });
+    // event handler for new connections
+
+    // handlers for the different forms in the page
+    // these send data to the server in a variety of ways
+     $('#start-serial').click(function(event) {
+        socket.emit('Serial start');
+        return false;
+    });
+
+    $('#send-data').click(function(event) {
+        console.log($('#serial-data').val());
+        socket.emit('Serial event', {data: $("#serial-dada").val()});
+        return false;
+    });
+    $('#close-serial').click(function(event) {
+        socket.emit('close');
+        return false;
+    });
+
     $("#launch-btn").click(function(){
+        socket.emit('close');
         if(file_manager.user_file!=null){
             if(file_manager.user_file.active){
                 file_manager.loadFile(file_manager.user_file);
@@ -150,7 +190,6 @@ $(document).ready(function(){
                 break;
             }
         }
-
     });
 
 });

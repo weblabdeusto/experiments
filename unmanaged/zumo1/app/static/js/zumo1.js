4319a14279d2c94dbf48ae9ed0eeecf2fec89990
gabimart <gabi.martinez@deusto.es>
2016-05-03 19:52:32 +0200

    Send to serial working

--- unmanaged/zumo1/app/static/js/zumo1.js
+++ unmanaged/zumo1/app/static/js/zumo1.js
@@ -230,13 +230,16 @@ $(document).ready(function(){
                 launch_btn.prop("disabled", false);
                 status_div.html("<p>Ready!!</p>")
             }
-            var messages = msg.data.split("\n");
-            for (var i = 0; i <= messages.length; i++) {
-                if (messages[i] != undefined) {
-                    serialDiv.append('<p>' + messages[i] + '</p>');
-                    serialDiv.scrollTop(serialDiv.children().length * 1000)
+            else{
+                var messages = msg.data.split("\n");
+                for (var i = 0; i <= messages.length; i++) {
+                    if (messages[i] != undefined) {
+                        serialDiv.append('<p>' + messages[i] + '</p>');
+                        serialDiv.scrollTop(serialDiv.children().length * 1000)
+                    }
                 }
             }
+
         });
 
         $("#button_finish").click(function () {

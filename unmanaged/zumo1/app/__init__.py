aa56ba5a369bed389a5aab769b63163efbc03eef
gabimart <gabi.martinez@deusto.es>
2016-05-03 20:03:11 +0200

    logs

--- unmanaged/zumo1/app/__init__.py
+++ unmanaged/zumo1/app/__init__.py
@@ -30,7 +30,7 @@ if not app.debug:
     import logging
     from logging.handlers import RotatingFileHandler
     file_handler = RotatingFileHandler('logs/zumo1.log', 'a',
-                                       1 * 1024 * 1024, 10)
+                                       ar1 * 1024 * 1024, 10)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))

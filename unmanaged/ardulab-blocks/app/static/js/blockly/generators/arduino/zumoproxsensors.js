48968c0b94c9225843d17e7417336db6884eebae
gabimart <gabi.martinez@deusto.es>
2016-05-05 23:11:06 +0200

    authentication error fixed?

--- unmanaged/ardulab-blocks/app/static/js/blockly/generators/arduino/zumoproxsensors.js
+++ unmanaged/ardulab-blocks/app/static/js/blockly/generators/arduino/zumoproxsensors.js
@@ -43,7 +43,7 @@ Blockly.Arduino['countsGeneric'] = function(block) {
         code += 'LeftWith';
     }
     else{
-        code += 'CenterWith';
+        code += 'FrontWith';
     }
 
     if(led='RIGHT'){

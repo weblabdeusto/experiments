db2ff2dbd7c30a746a67584c6c3994cc9d1cc184
gabimart <gabi.martinez@deusto.es>
2016-05-11 12:17:26 +0200

    powerManager v0.1

--- firmwares/power-manager/config.py
+++ firmwares/power-manager/config.py
@@ -7,5 +7,5 @@ if DEBUG:
     ]
 else:
     labs = [
-        {'name': 'zumoline', 'ip':'192.168.0.130', 'path':'/labs/zumoline', 'relay':0 ,'lastDown': None}
+        {'name': 'zumoline', 'ip':'192.168.0.130', 'path':'/labs/zumoline/test', 'relay':0 ,'lastDown': None}
     ]

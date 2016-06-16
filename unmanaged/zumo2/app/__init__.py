e6f76322c7d4f18c5afcb51492304195d637c114
gabimart <gabi.martinez@deusto.es>
2016-06-16 18:46:16 +0200

    .

--- unmanaged/zumo2/app/__init__.py
+++ unmanaged/zumo2/app/__init__.py
@@ -24,7 +24,7 @@ socketio = SocketIO(app, async_mode='eventlet', resource = "/labs/zumoline/socke
 if not app.debug:
     import logging
     from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('logs/zumo1.log', 'a',
+    file_handler = RotatingFileHandler('logs/zumo.log', 'a',
                                        1 * 1024 * 1024, 10)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(logging.Formatter(

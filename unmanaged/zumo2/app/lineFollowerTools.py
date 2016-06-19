cb715b89d043b7fd58a7b308b013738c75d9d20f
gabimart <gabi.martinez@deusto.es>
2016-06-19 16:51:55 +0200

    rfid reader

--- unmanaged/zumo2/app/lineFollowerTools.py
+++ unmanaged/zumo2/app/lineFollowerTools.py
@@ -13,9 +13,8 @@ class RFID_Reader(object):
 
     def read(self):
         out = ""
-        buff_len = self.serial.inWaiting()
-        if buff_len != 0:
-            out += self.serial.read(buff_len)
+        if self.serial.inWaiting():
+            out += self.serial.read(12)
             if out!="":
                 return True, out[1:11]
             else:
@@ -61,7 +60,7 @@ class Chrono(object):
                     self.socketio.emit('Chrono event',
                         {'data':str(response)},
                         broadcast=True)
-            sleep(0.1)
+            sleep(0.2)
 
 
     def stopChrono(self):

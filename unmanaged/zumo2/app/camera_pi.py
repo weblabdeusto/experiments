e4f054c85fea607db6408be3163f37a219f48e3f
gabimart <gabi.martinez@deusto.es>
2016-06-20 18:50:20 +0200

    pi camera added

--- unmanaged/zumo2/app/camera_pi.py
+++ unmanaged/zumo2/app/camera_pi.py
@@ -49,6 +49,6 @@ class Camera(object):
 
                 # if there hasn't been any clients asking for frames in
                 # the last 10 seconds stop the thread
-                if time.time() - cls.last_access > 10:
+                if time.time() - cls.last_access > 5:
                     break
         cls.thread = None

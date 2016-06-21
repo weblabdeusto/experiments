0aaedb2581e2c53e29a7928d571f01aca176ae36
gabimart <gabi.martinez@deusto.es>
2016-06-21 10:22:31 +0200

    picamera

--- unmanaged/zumo2/app/camera_pi.py
+++ unmanaged/zumo2/app/camera_pi.py
@@ -56,10 +56,12 @@ class Camera(object):
                     # if there hasn't been any clients asking for frames in
                     # the last 10 seconds stop the thread
                     if time.time() - cls.last_access > 3:
+                        print 'No client asking for camera'
                         break
 
             cls.camera.close()
             cls.thread = None
         except:
+            print 'Camera exception'
             cls.camera.close()
             cls.thread = None

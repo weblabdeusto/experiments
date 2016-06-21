69d06f12639bd9a5ce69eda379abad2b6778b2bd
gabimart <gabi.martinez@deusto.es>
2016-06-21 11:09:06 +0200

    picamera

--- unmanaged/zumo2/app/views.py
+++ unmanaged/zumo2/app/views.py
@@ -139,6 +139,7 @@ def sendSerial():
 def gen(camera):
     """Video streaming generator function."""
     while True:
+        time.sleep(0.2)
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')

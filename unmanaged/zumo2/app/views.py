fb1d5fdb036682f0e23eb2b9bec8886609c4f1ae
gabimart <gabi.martinez@deusto.es>
2016-06-23 12:00:00 +0200

    improve camera

--- unmanaged/zumo2/app/views.py
+++ unmanaged/zumo2/app/views.py
@@ -152,6 +152,8 @@ def gen(camera):
 @zumo.route('/video_feed')
 @check_permission
 def video_feed():
+    if not camera.stop:
+        camera.close()
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')

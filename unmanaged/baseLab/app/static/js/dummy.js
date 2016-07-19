9104277a63c2e75a2aae99d17bfb0451f2d43acb
gabimart <gabi.martinez@deusto.es>
2016-07-19 12:12:13 +0200

    [baseLab]Camera module added

--- unmanaged/baseLab/app/static/js/dummy.js
+++ unmanaged/baseLab/app/static/js/dummy.js
@@ -4,14 +4,19 @@
 
 
 $(document).ready(function(){
+    //This timeout is added for ensuring socket connection is performed before user functions are declared
+    setTimeout(function(){
+        //Add experiment functions here
 
-    //Data sending example
-    $("#test-button").click(function(){
-        socket.emit('button',{data: 'Example button pushed'});
-    });
+        //Data sending example
+        $("#test-button").click(function(){
+            socket.emit('button',{data: 'Example button pushed'});
+        });
 
-    //Data reciving example
-    socket.on('Controller event',function(msg){
-        console.log(msg.data);
-    });
+        //Data reciving example
+        socket.on('Controller event',function(msg){
+            console.log(msg.data);
+        });
+
+    },1000);
 });

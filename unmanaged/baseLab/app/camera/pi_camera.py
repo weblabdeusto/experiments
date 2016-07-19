4605383460b1c498345252c7d6e2103c11977bd7
gabimart <gabi.martinez@deusto.es>
2016-07-19 11:07:37 +0200

    [baseLab]Camera module added

---
+++ unmanaged/baseLab/app/camera/pi_camera.py
@@ -0,0 +1,62 @@
+import io
+import threading
+import time
+import cv2
+from PIL import Image
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+
+
+class Camera(object):
+    thread = None  # background thread that reads frames from camera
+    frame = None  # current frame is stored here by background thread
+    last_access = 0  # time of last client access to the camera
+
+
+    def initialize(self):
+        if Camera.thread is None:
+            # start background frame thread
+            Camera.thread = threading.Thread(target=self._thread)
+            Camera.thread.start()
+
+            # wait until frames start to be available
+            while self.frame is None:
+                time.sleep(0.1)
+
+    def get_frame(self):
+        Camera.last_access = time.time()
+        self.initialize()
+        return self.frame
+
+    @classmethod
+    def _thread(cls):
+        camera =  PiCamera()
+        camera.resolution = (320, 240)
+        camera.hflip = True
+        camera.vflip = True
+        rawCapture = PiRGBArray(camera, size=(320, 240))
+
+        time.sleep(1)
+        stream = io.BytesIO()
+        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+            time.sleep(0.2)
+            img = frame.array
+            imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+            jpg = Image.fromarray(imgRGB)
+            jpg.save(stream,'JPEG')
+
+            # store frame
+            stream.seek(0)
+            cls.frame = stream.read()
+
+            # reset stream for next frame
+            stream.seek(0)
+            stream.truncate()
+
+            # if there hasn't been any clients asking for frames in
+            # the last 10 seconds stop the thread
+            if time.time() - cls.last_access > 10:
+                break
+
+        cls.thread = None
---
+++ unmanaged/baseLab/app/camera/views.py
@@ -0,0 +1,37 @@
+from app.camera.config import CAMERA
+from flask import Response
+import time
+from app.sessionManager.tools import check_permission
+from app import video
+
+if CAMERA == 'pi_camera':
+    from app.camera.pi_camera import Camera
+else:
+    from app.camera.web_camera import Camera
+
+camera = Camera()
+
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        time.sleep(0.2)
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+
+
+@video.route('/video_feed')
+@check_permission
+def video_feed():
+    global camera
+
+    if not camera.stop:
+        print 'camera is running'
+        camera.close()
+        print 'camera is clossed'
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
